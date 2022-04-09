99b1a68f59156515cff10163f7546b6bf1774b78
kazi0021 <kazi0021@d.umn.edu>
2022-04-09 12:22:32 -0500

    version 1.6

--- lib/vars.dart
+++ lib/vars.dart
@@ -1,7 +1,7 @@
 String base_url = 'http://131.212.41.11:50000';
 // String base_url = 'http://127.0.0.1:8000';
 
-String app_version = "Version 1.5";
+String app_version = "Version 1.6";
 
 
 class ErrorStrings{
