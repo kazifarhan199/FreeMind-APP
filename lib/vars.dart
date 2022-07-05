acc4695e899411231695b408e3f237d3628ae034
Olivia Brazier <brazi061@d.umn.edu>
2022-07-05 10:46:42 -0500

    Changed address

--- lib/vars.dart
+++ lib/vars.dart
@@ -1,4 +1,4 @@
-String base_url = 'http://131.212.41.11:50000';
+String base_url = 'http://131.212.41.25:55000';
 // String base_url = 'http://127.0.0.1:8000';
 
 String app_version = "Version 1.6";
