6ddd6948e3866b5ac9d0757ca1c79bc468f7bf70
kazi0021 <kazi0021@d.umn.edu>
2022-09-12 09:08:12 -0500

    debug: Information strings not defined

--- lib/vars.dart
+++ lib/vars.dart
@@ -1,11 +1,11 @@
+String base_url = 'https://131.212.41.11:57000';
 // String base_url = 'http://131.212.41.11:50000';
-String base_url = 'http://10.0.2.2:8000';
+// String base_url = 'http://10.0.2.2:8000';
 // String base_url = 'http://127.0.0.1:8000';
 
 String app_version = "Version 1.6";
 
-
-class ErrorStrings{
+class ErrorStrings {
   static String group_name_needed = 'Please provide a name for the group';
   static String email_needed = 'Please provide an email';
   static String name_needed = 'Please provide a name';
@@ -20,12 +20,16 @@ class ErrorStrings{
   static String loggedout = 'Logged out please logout and login again';
 }
 
-class InfoStrings{
+class InfoStrings {
   static String title_info = "What are you doing for yourself today ?";
   static String logout_info = "Are you sure you want to logout ?";
-  static String survey_label_info = "How would you rate the following behaviors ...";
-  static String survey_cuppled_info = "How would you rate the following recommendation ...";
-  static String survey_info = "Help us to help you by telling us about your linkings\n\nThis data will be used to recommend you activities to improve your self care\n";
-  static String password_helper = "Help us protect you, make sure your password \n\n• Is at least 8 character long\n• Contains at least one number\n• Contains at least one special character\n";
-  
+  static String survey_label_info =
+      "How would you rate the following behaviors ...";
+  static String survey_cuppled_info =
+      "How would you rate the following recommendation ...";
+  static String survey_info =
+      "Help us to help you by telling us about your linkings\n\nThis data will be used to recommend you activities to improve your self care\n";
+  static String password_helper =
+      "Help us protect you, make sure your password \n\n• Is at least 8 character long\n• Contains at least one number\n• Contains at least one special character\n";
+  static String link_info = "Please provide link to more information";
 }
