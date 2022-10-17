533dc4b5a8bcddff62067aead1235236c4791afd
kazi0021 <kazi0021@d.umn.edu>
2022-10-17 08:28:38 -0500

    refactored: refactored the wrapper.dart file (removed some unused functions and commented the file)

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -10,24 +10,25 @@ import 'package:social/screans/utils/loading.dart';
 import 'package:social/screans/utils/noGroup.dart';
 
 class Wrapper extends StatefulWidget {
-  const Wrapper({ Key? key }) : super(key: key);
+  const Wrapper({Key? key}) : super(key: key);
 
   @override
   State<Wrapper> createState() => _WrapperState();
 }
 
 class _WrapperState extends State<Wrapper> {
-  bool loading=false;
+  bool loading = false;
 
+  // false if user is not logged in
   bool isUserLoggedIn() {
     if (Hive.box('userBox').isNotEmpty) {
       User user = Hive.box('userBox').getAt(0) as User;
-      if (user.id == 0){
+      if (user.id == 0) {
         return false;
       }
       updateUserProfile();
       user = Hive.box('userBox').getAt(0) as User;
-      if (user.id == 0){
+      if (user.id == 0) {
         return false;
       }
       return true;
@@ -35,28 +36,27 @@ class _WrapperState extends State<Wrapper> {
     return false;
   }
 
-  bool userHasGroup(){
-    User user = Hive.box('userBox').getAt(0) as User;
-    if (user.gid == 0){
-      return false;
-    }
-    return true;
-  }
-
+  // Sync local user profile with one on the server
   updateUserProfile() async {
-    if (mounted) setState(() => loading=true);
+    if (mounted) setState(() => loading = true);
     User user = Hive.box('userBox').getAt(0) as User;
-    try{
+    try {
       User.profile();
-    }catch(e) {
-      if (mounted) errorBox(context: context, errorTitle: "Error", error: e.toString().substring(11));
+    } catch (e) {
+      if (mounted)
+        errorBox(
+            context: context,
+            errorTitle: "Error",
+            error: e.toString().substring(11));
     }
-    if (mounted) setState(() => loading=false);
+    if (mounted) setState(() => loading = false);
   }
 
   @override
   Widget build(BuildContext context) {
-    // return Loading(loading: loading,child: isUserLoggedIn()? user  HasGroup()?Home():NoGroup():Login());
-    return Loading(loading: loading,child: isUserLoggedIn()? true?Home():NoGroup():Login());
+    // Show loading symbol till trying to figure out is user is logged in then
+    // return login page if not loggedin else return the home page
+    return Loading(
+        loading: loading, child: isUserLoggedIn() ? Home() : Login());
   }
 }
