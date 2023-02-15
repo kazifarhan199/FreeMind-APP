4abc00d1f1e89c958b05174d57b59d8b11cbeac3
Farhanuddin Kazi <kazifarhan199@gmail.com>
2023-02-15 14:09:16 -0600

    feature: Added lls and changed url

--- lib/vars.dart
+++ lib/vars.dart
@@ -1,4 +1,5 @@
-String base_url = 'http://131.212.41.11:59000';
+String base_url = 'https://csgrad08.d.umn.edu';
+// String base_url = 'http://131.212.41.11:59000';
 // String base_url = 'http://131.212.41.11:50000';
 // String base_url = 'http://10.0.2.2:8000';
 // String base_url = 'http://127.0.0.1:8000';
